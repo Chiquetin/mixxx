354e6e4453ee6c329239c9c946039ff4fa6f9287
Chiquetin <dominik.schmundt@web.de>
2025-05-31 22:12:07 +0200

    CortinaMode: fix broken if-statement

--- src/library/autodj/autodjprocessor.cpp
+++ src/library/autodj/autodjprocessor.cpp
@@ -818,7 +818,7 @@ void AutoDJProcessor::playerPositionChanged(DeckAttributes* pAttributes,
     if (m_eState == ADJ_LEFT_INTRO_FADE_IN || m_eState == ADJ_RIGHT_INTRO_FADE_IN) {
         // ignore updates from the non-fading deck updates during INTRO_FADE_IN
         if ((m_eState == ADJ_LEFT_INTRO_FADE_IN && thisDeck->isRight()) ||
-                (m_eState == ADJ_RIGHT_INTRO_FADE_IN && thisDeck->isLeft()) {
+                (m_eState == ADJ_RIGHT_INTRO_FADE_IN && thisDeck->isLeft())) {
             qDebug() << thisDeck->group << "not relevant during fade in";
             return;
         }
