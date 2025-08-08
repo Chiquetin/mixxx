5a7c90e861918a334a1e64b8f6a6812a34a3fbcf
Chiquetin <dominik.schmundt@web.de>
2025-08-08 18:04:15 +0200

    [change] logic of CortinaTransitionMode
    All code for handling CortinaTransitionMode is now in two places:
    - playerPositionChanged
    - fadeNow
    The code for the transition has been adjusted to be the same in both
    positions.
    Fading in has been simlified to only look at the current track.
    Fading out has been added and work on outro markes
    Open items are:
    - Current version does not cue to he correct start position
    - Current version does not apply fade-in to the first track
    - Use of transition time for fading when no intro/outro not implemented

--- src/library/autodj/autodjprocessor.h
+++ src/library/autodj/autodjprocessor.h
@@ -154,8 +154,7 @@ class AutoDJProcessor : public QObject {
         ADJ_IDLE = 0,
         ADJ_LEFT_FADING,
         ADJ_RIGHT_FADING,
-        ADJ_LEFT_INTRO_FADE_IN,
-        ADJ_RIGHT_INTRO_FADE_IN,
+        ADJ_SOCIAL_FADE_IN,
         ADJ_ENABLE_P1LOADED,
         ADJ_ENABLE_P1PLAYING,
         ADJ_DISABLED
